41894d971a21f6832f332cb52c98d66f230a7786
jtse9 <54727553+jtse9@users.noreply.github.com>
2019-09-22 20:22:05 -0700

    Add files via upload

---
+++ orbit2.py
@@ -0,0 +1,42 @@
+from math import sqrt
+from drawtraj import drawtraj
+def force(x,y,m,mstar):
+    r2=x**2+y**2
+    r32=r2*sqrt(r2)
+    fx=-x*m*mstar/r32
+    fy=-y*m*mstar/r32
+    return fx,fy
+def integrate(x,y,vx,vy,fx,fy,m,dt):
+    ax,ay=fx/m,fy/m
+    vx+=ax*dt
+    vy+=ay*dt
+    x+=vx*dt
+    y+=vy*dt
+    return x,y,vx,vy
+# Main part of the program
+mstar=100
+m=1
+nsteps=100000
+dt=0.01
+r=50
+x,y=0,r
+vx,vy=1.2,0
+
+xp,yp=0,0
+vxp,vyp=0,0
+#fxp= (-1)*fx
+#fyp=(-1)*fy
+trajx,trajy=[],[]
+starx = []
+stary=[]
+for t in range(nsteps):
+    fx,fy=force(x,y,m,mstar)
+    x,y,vx,vy=integrate(x,y,vx,vy,fx,fy,m,dt)
+    trajx.append(x)
+    trajy.append(y)
+for t in range(nsteps):
+    fxp,fyp=force(x,y,m,mstar)
+    xp,yp,vxp,vyp=integrate(xp,yp,vxp,vyp,fxp,fyp,mstar,dt)
+    starx.append(xp)
+    stary.append(yp)
+drawtraj(trajx,trajy,6*r)
